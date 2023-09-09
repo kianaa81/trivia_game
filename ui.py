1a23c2d99f174894890f8a5d4248a54792549c08
Kiana <salmasikiana@gmail.com>
2023-09-09 14:37:42 -0600

    Created a canvas

--- ui.py
+++ ui.py
@@ -13,4 +13,15 @@ class QuizInterface:
         self.score_label = Label(text="Score: 0", fg=THEME_COLOR)
         self.score_label.grid(row=0, column=1)
 
+        # Create a canvas
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            text="Some questions here",
+            fill=THEME_COLOR,
+            font=("Ariel", 20, "italic"),
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
         self.window.mainloop()
