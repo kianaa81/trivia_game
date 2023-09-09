4d91183570bb0c9a74ed35e8cdedbf9e55cd6dd4
Kiana <salmasikiana@gmail.com>
2023-09-09 14:30:01 -0600

    Created a score label

--- ui.py
+++ ui.py
@@ -7,5 +7,10 @@ class QuizInterface:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        # Create a label on the top
+        self.score_label = Label(text="Score: 0", fg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
 
         self.window.mainloop()
